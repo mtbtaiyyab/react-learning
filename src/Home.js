5fb53972b3762410a0297f709acac453237afcbd
Mohd Taiyyab <taiyyabmohammad11@gmail.com>
2024-05-04 23:14:37 +0530

    Painting List

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,39 @@
 import { useState } from "react";
 
+
 const Home = () => {
-    const[name, setName] = useState('Taiyyab');
-    const[age, setAge] = useState(30);
-    const handleClick = (e) =>{
-        console.log('AAAAAAAAAAAAAaa' + e.target);
-        let printName = name;
-        let printAge = age;
-        printName = name === 'Azeem' ? 'Taiyyab' : 'Azeem';
-        printAge = age === 30 ? 50 : 30;
-        setName(printName);
-        setAge(printAge);
-    }
+    // USESTATE EXAMPLE
+    // const[name, setName] = useState('Taiyyab');
+    // const[age, setAge] = useState(30);
+    // const handleClick = (e) =>{
+    //     console.log('AAAAAAAAAAAAAaa' + e.target);
+    //     let printName = name;
+    //     let printAge = age;
+    //     printName = name === 'Azeem' ? 'Taiyyab' : 'Azeem';
+    //     printAge = age === 30 ? 50 : 30;
+    //     setName(printName);
+    //     setAge(printAge);
+    // }
+    const [blogs, setBlogs] = useState([
+        {title: 'My New website', body: 'loremipsum...',author:'mario', id:1},
+        {title: 'Welcome Website', body: 'loremipsum...', author:'Dino', id:2},
+        {title: 'Web Dev Server', body: 'loremipsum...',author:'James', id:3 }
+    ]);
     return ( 
         <div className="home">
-            <h2>HomePage</h2>
-            <button onClick={handleClick}>Click Me</button>
+            {
+                blogs.map((blog) =>(
+                    <div className="blog-preview" key={blog.id}>
+                        <h2>{blog.title}</h2>
+                        <p>Written By {blog.author}</p>
+                    </div>
+                ))
+            }
+            {/*
+            useState Example <button onClick={handleClick}>Click Me</button>
 
-            <p>{name} is {age} years old.</p>
+            <p>{name} is {age} years old.</p> */}
+            
         </div>
      );
 }
