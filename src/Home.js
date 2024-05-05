a24bef6d5d4b9b274084649d3bb52ba3e54d803f
Mohd Taiyyab <taiyyabmohammad11@gmail.com>
2024-05-05 23:33:34 +0530

    Using useEffect Dependency Array

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 
@@ -20,13 +20,22 @@ const Home = () => {
         { title: 'Welcome Website', body: 'loremipsum...', author: 'Dino', id: 2 },
         { title: 'Web Dev Server', body: 'loremipsum...', author: 'Mario', id: 3 }
     ]);
+    const [name, setName] = useState('Jerry');
+    useEffect(() => {
+        console.log('Hello form useEffects');
+        console.log(blogs);
+        console.log('name');
+    }, [name]);
     const handleBlog = (id) => {
         const filteredBlog = blogs.filter(blog => blog.id !== id);
         setBlogs(filteredBlog);
     }
+
     return (
         <div className="home">
-            <BlogList sect={blogs} title="Blog List" deleteBlog = {handleBlog} />
+            <BlogList sect={blogs} title="Blog List" deleteBlog={handleBlog} />
+            <button onClick={() => { setName('Tom') }}>Change Name</button>
+            <p>{name}</p>
             {/* Reusing Components */}
             {/* <BlogList sect={blogs.filter((blog) => (blog.author === 'Mario'))} title="Filtered Blog List"/> */}
             {/*
