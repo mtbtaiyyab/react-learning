be2c944d7957933a6603b793cfcfc0e759ae6d7e
Mohd Taiyyab <taiyyabmohammad11@gmail.com>
2024-05-07 22:46:23 +0530

    Handling Error

--- src/Home.js
+++ src/Home.js
@@ -4,19 +4,35 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+    const [errror, disableError] = useState(null);
+
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-           return res.json();
-        })
-        .then(data => {
-            console.log(data);
-            setBlogs(data);
-        });
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if(!res.ok){
+                        throw Error('Could not connect to the server....');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setLoading(false);
+                    disableError(null);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                    disableError(err.message);
+                    setLoading(false);
+                });
+        }, 1000);
     }, []);
 
     return (
         <div className="home">
+            {errror && <div>{errror}</div>}
+            {isLoading && <div>Loading...</div>}
             {blogs && <BlogList sect={blogs} title="Blog List" />}
         </div>
     );
