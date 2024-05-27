01770c2e467769e7b143537a1f23f5384b89871d
Mohd Taiyyab <taiyyabmohammad11@gmail.com>
2024-05-27 09:28:01 +0530

    Populating data in Blog Details

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,22 @@
 import { useParams } from "react-router-dom/cjs/react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
-    const {id} = useParams();
-    return ( 
+    const { id } = useParams();
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    return (
         <div className="blog-details">
-            <h2>BlogDetails : {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written By {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
-     );
+    );
 }
- 
+
 export default BlogDetails;
