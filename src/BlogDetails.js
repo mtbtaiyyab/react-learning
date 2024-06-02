b1b6e6651d7ebd9e7896a0361c2b7e9d42e7e331
Mohd Taiyyab <taiyyabmohammad11@gmail.com>
2024-06-02 09:58:20 +0530

    Delete Functionality

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,17 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom";
+import { useParams, useHistory } from "react-router-dom/cjs/react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+    const deleteBlog = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id,{
+            method:'DELETE'
+        }).then(() => {
+            history.push('/');
+        });
+    }
     return (
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
@@ -13,6 +21,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written By {blog.author}</p>
                     <div>{blog.detail}</div>
+                    <button onClick={deleteBlog}>DELETE</button>
                 </article>
             )}
         </div>
